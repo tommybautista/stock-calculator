a8f115cc0240ebea81385b430a7e8efea71eb0dc
tommybautista <100245701+tommybautista@users.noreply.github.com>
2022-04-15 20:08:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function myFunction() {
+    var buyPower = document.getElementById("buying-power").value;
+    var stockPrice = document.getElementById("stock-price").value;
+    var net = document.getElementById("net").value;
+    var amount = document.getElementById("amount").innerHTML;
+    var targetPrice = document.getElementById("target-price").innerHTML;
+    var stopLossDol = document.getElementById("stop-loss-dol").value;
+    var stopLossPer = document.getElementById("stop-loss-per").value;
+    var tradingDays = document.getElementById("trading-days").value;
+    document.querySelector(".amount").innerHTML = (buyPower / stockPrice);
+}
+
+function myFunction1() {
+    var buyPower = document.getElementById("buying-power").value;
+    var stockPrice = document.getElementById("stock-price").value;
+    var net = document.getElementById("net").value;
+    var amount = document.getElementById("amount").innerHTML;
+    var targetPrice = document.getElementById("target-price").innerHTML;
+    var stopLossDol = document.getElementById("stop-loss-dol").value;
+    var stopLossPer = document.getElementById("stop-loss-per").value;
+    var tradingDays = document.getElementById("trading-days").value;
+    document.querySelector(".margin").innerHTML = (targetPrice - (stockPrice * amount)).toFixed(2);
+    document.querySelector(".target-price").innerHTML = (((amount * stockPrice) + (net)) / (amount)).toFixed(2)
+}
+
+function myFunction2() {
+    var buyPower = document.getElementById("buying-power").value;
+    var stockPrice = document.getElementById("stock-price").value;
+    var net = document.getElementById("net").value;
+    var amount = document.getElementById("amount").innerHTML;
+    var targetPrice = document.getElementById("target-price").innerHTML;
+    var stopLossDol = document.getElementById("stop-loss-dol").value;
+    var stopLossPer = document.getElementById("stop-loss-per").value;
+    var tradingDays = document.getElementById("trading-days").value;
+    document.querySelector(".stop-limit-order-dol").innerHTML = (((amount * stockPrice) - stopLossDol) / amount).toFixed(2);
+}
+
+function myFunction3() {
+    var buyPower = document.getElementById("buying-power").value;
+    var stockPrice = document.getElementById("stock-price").value;
+    var net = document.getElementById("net").value;
+    var amount = document.getElementById("amount").innerHTML;
+    var targetPrice = document.getElementById("target-price").innerHTML;
+    var stopLossDol = document.getElementById("stop-loss-dol").value;
+    var stopLossPer = document.getElementById("stop-loss-per").value;
+    var tradingDays = document.getElementById("trading-days").value;
+    document.querySelector(".stop-limit-order-per").innerHTML = (stockPrice - ((stockPrice * stopLossPer))).toFixed(2);
+}
+
+function myFunction4() {
+    var buyPower = document.getElementById("buying-power").value;
+    var stockPrice = document.getElementById("stock-price").value;
+    var net = document.getElementById("net").value;
+    var amount = document.getElementById("amount").innerHTML;
+    var targetPrice = document.getElementById("target-price").innerHTML;
+    var stopLossDol = document.getElementById("stop-loss-dol").value;
+    var stopLossPer = document.getElementById("stop-loss-per").value;
+    var tradingDays = document.getElementById("trading-days").value;
+    document.querySelector(".daily-avg").innerHTML = (net / tradingDays).toFixed(2);
+}
